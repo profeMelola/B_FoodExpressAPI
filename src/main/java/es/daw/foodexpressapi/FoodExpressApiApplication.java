313975809027f0122109e4f2311817c25d65f9c9
melola <melola@educa.madrid.org>
2025-11-17 19:37:47 +0100

    Primer commit

---
+++ src/main/java/es/daw/foodexpressapi/FoodExpressApiApplication.java
@@ -0,0 +1,13 @@
+package es.daw.foodexpressapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodExpressApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodExpressApiApplication.class, args);
+    }
+
+}
